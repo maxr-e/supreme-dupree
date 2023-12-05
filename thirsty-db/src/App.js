650fb2f1ab55d3f4c802a6604fdbe079823f9a50
Maximilian Royal-Eisenberg <maxroyal.eisenberg@gmail.com>
2023-12-04 21:50:01 -0800

    prepped package.json for deployment

--- thirsty-db/src/App.js
+++ thirsty-db/src/App.js
@@ -9,6 +9,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>
+          Login in here. Then, let's check out two pages- a map and a submission page.
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
